c0d1ed7ddfe9374dd81febd6cbe2321cc58247ca
Will van Noordt <willvanzero@gmail.com>
2021-04-30 01:24:51 +0100

    minor

--- validation/postprocess.m
+++ validation/postprocess.m
@@ -2,11 +2,11 @@ clear
 clc
 close all
 
-data = csvread('../series/kinetic.csv');
-%compdata = csvread('reference.csv');
+data = csvread('../series/enstrophy.csv');
+compdata = csvread('reference.csv');
 figure
 hold on
 plot(data(:, 1), data(:, 2)/data(1, 2))
-%plot(compdata(:, 1), compdata(:, 2)/compdata(1, 2))
-%legend('me', 'them');
-%axis([0 20 0 14]);
+plot(compdata(:, 1), compdata(:, 2)/compdata(1, 2))
+legend('me', 'them');
+axis([0 20 0 14]);
