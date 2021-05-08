6ec14ce2cbd95b33f6253b765a3eaa9dc29a356e
Will van Noordt <willvanzero@gmail.com>
2021-05-08 19:34:48 +0100

    begin refactor output

--- OutputData.hpp
+++ OutputData.hpp
@@ -6,10 +6,19 @@
 using cmf::ZFill;
 using cmf::strformat;
 using cmf::print;
+std::string spaces(int i)
+{
+    std::string output = "";
+    if (i==0) return output;
+    while ((output+= " ").length()<i) {}
+    return output;
+}
 void OutputData(int nt, cmf::CartesianMeshArray& array, InputParams& params)
 {
     int iGuards = params.guardOutput?1:0;
     double multGuard = 1.0 + iGuards;
+    int numBlocksWritten = 0;
+    std::string filenamePrototype = "output/block_{}_proc_{}_nt_{}.vtk";
     for (int process = 0; process < cmf::globalGroup.Size(); process++)
     {
         if (cmf::globalGroup.Rank()==process)
@@ -19,19 +28,30 @@ void OutputData(int nt, cmf::CartesianMeshArray& array, InputParams& params)
             for (auto lb: array)
             {
                 print(" >> block ", p);
+                
                 auto info = array.GetBlockInfo(lb);
                 cmf::BlockArray<double, 1> block = array[lb];
-                std::string filename = strformat("output/block_{}_proc_{}_nt_{}.vtk", ZFill(p, 4), ZFill(cmf::globalGroup.Rank(), 4), ZFill(nt, 7));
-                std::ofstream myfile;
-                myfile.open(filename.c_str());
-                myfile << "# vtk DataFile Version 3.0" << std::endl;
-                myfile << "vtk output" << std::endl;
-                myfile << "ASCII" << std::endl;
-                myfile << "DATASET STRUCTURED_POINTS" << std::endl;
-                myfile << "DIMENSIONS ";
+                std::string filename = strformat(filenamePrototype, ZFill(p, 4), ZFill(cmf::globalGroup.Rank(), 4), ZFill(nt, 7));
+                
                 int ni = block.imax - block.imin + 2*iGuards*block.exchangeI;
                 int nj = block.jmax - block.jmin + 2*iGuards*block.exchangeJ;
                 int nk = block.kmax - block.kmin + 2*iGuards*block.exchangeK;
+                int numCells = ni*nj*nk;
+                int numPoints = (ni+1)*(nj+1)*(nk+1);
+                
+                std::ofstream myfile;
+                myfile.open(filename.c_str());
+                numBlocksWritten++;
+                myfile << "<VTKFile type=\"UnstructuredGrid\">" << std::endl;
+                myfile << spaces(4) <<"<UnstructuredGrid>" << std::endl;
+                myfile << spaces(8) <<"<Piece NumberOfPoints=\"" << numPoints << "\" NumberOfCells=\"" << numCells << "\">" << std::endl;
+                myfile << spaces(12) << "<PointData>" << "..." << "</PointData>" << std::endl;
+                myfile << spaces(12) << "<CellData>" << "..." << "</CellData>" << std::endl;
+                myfile << spaces(12) << "<Points>" << "..." << "</Points>" << std::endl;
+                myfile << spaces(12) << "<Cells>" << "..." << "</Cells>" << std::endl;
+                myfile << spaces(8) << "</Piece>" << std::endl;
+                myfile << spaces(4) << "</UnstructuredGrid>" << std::endl;
+                myfile << "</VTKFile>" << std::endl;
                 myfile << (ni+1) << " ";
                 myfile << (nj+1) << " ";
                 myfile << (nk+1) << std::endl;
@@ -64,6 +84,28 @@ void OutputData(int nt, cmf::CartesianMeshArray& array, InputParams& params)
             }
         }
         cmf::globalGroup.Synchronize();
+        
+        int* allNumBlocksWritten = cmf::globalGroup.SharedValues(numBlocksWritten);
+        int totalNumBlocksWritten = cmf::globalGroup.Sum(numBlocksWritten);
+        
+        if (cmf::globalGroup.IsRoot())
+        {
+            std::ofstream myfile;
+            std::string metaFile = "output/db_" + ZFill(nt, 7) + ".visit";
+            myfile.open(metaFile.c_str());
+            myfile << "!NBLOCKS " << totalNumBlocksWritten << std::endl;
+            for (int i = 0; i < cmf::globalGroup.Size(); i++)
+            {
+                int rank = i;
+                int numBlocksLoc = allNumBlocksWritten[i];
+                for (int j = 0; j < numBlocksLoc; j++)
+                {
+                    std::string filename = strformat(filenamePrototype, ZFill(j, 4), ZFill(i, 4), ZFill(nt, 7));
+                    myfile << filename << std::endl;
+                }
+            }
+            myfile.close();
+        }
     }
 }
 
